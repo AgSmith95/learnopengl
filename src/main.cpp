7613f7e68f57e67fe0a0847c6bd85051158eda40
AgSmith <ivan.a.kornienko@gmail.com>
2020-05-06 01:20:08 +0300

    Hello Triangle
    Link: https://learnopengl.com/Getting-started/Hello-Triangle
    Draw 2 triangles - a rectangle
    
    + cleanup after last commit:
      - VBO buffer was bond 2 times
      - glVertexAttribPointer was called 2 times
      despite it wasn't necessary

--- src/main.cpp
+++ src/main.cpp
@@ -13,30 +13,39 @@ int main()
     window(); // not very elegant, yeah
 
     float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
     };
 
     // GENERATE VBO
     unsigned int VBO;
     glGenBuffers(1, &VBO);
-    // 0. copy our vertices array in a buffer for OpenGL to use
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // 1. then set the vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
-    glEnableVertexAttribArray(0);
+
+    // GENERATE EBO
+    // to draw 2 triangles - a rectangle
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
 
     // GENERATE VAO
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
+
+    // BIND BUFFERS
     // 1. bind Vertex Array Object
     glBindVertexArray(VAO);
     // 2. copy our vertices array in a buffer for OpenGL to use
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // 3. then set our vertex attributes pointers
+    // 3. copy our index array in a element buffer for OpenGL to use
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // 4. then set our vertex attributes pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
     glEnableVertexAttribArray(0);
 
@@ -129,7 +138,8 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glBindVertexArray(0);
         // render end
 
         // swap buffers
