c10688642568d404ca70450a8751051e056da442
AgSmith <ivan.a.kornienko@gmail.com>
2020-05-08 01:30:30 +0300

    Hello Triangle
    Link: https://learnopengl.com/Getting-started/Hello-Triangle
    Adding stuff from the solution
    mainly comments and explanations

--- src/main.cpp
+++ src/main.cpp
@@ -49,6 +49,23 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
     glEnableVertexAttribArray(0);
 
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex
+    // attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object
+    // IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO,
+    // but this rarely happens. Modifying other VAOs requires a call to glBindVertexArray anyways
+    // so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     // SHADERS
     // temporary variables for status
     int  success;
@@ -136,9 +153,14 @@ int main()
         // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        // use our shader
         glUseProgram(shaderProgram);
+        // as we only have a single VAO there's no need to bind it every time,
+        // but we'll do so to keep things a bit more organized
         glBindVertexArray(VAO);
+        //glDrawArrays(GL_TRIANGLES, 0, 6); // drawing triangles
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        // glBindVertexArray(0); // no need to unbind it every time
         glBindVertexArray(0);
         // render end
 
@@ -149,6 +171,10 @@ int main()
     }
     // RENDER LOOP end
 
+    // optional: de-allocate all resources once they've outlived their purpose:
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     return 0;
