9d8fd105ef0bd8151093b2a453f9336d64cb96d9
ivan.kornienko1 <Ivan.Kornienko1@refinitiv.com>
2020-06-01 20:48:14 +0300

    fixed a few windows warnings

--- src/libs/shaders_lib/src/Shader.cpp
+++ src/libs/shaders_lib/src/Shader.cpp
@@ -26,7 +26,7 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath) {
         vertexCode = vShaderStream.str();
         fragmentCode = fShaderStream.str();
     }
-    catch (std::ifstream::failure& e) {
+    catch (std::ifstream::failure&) {
         std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ\n"
                   << "    check files:\n"
                   << "        " << vertexPath << '\n'
--- src/mains/hello_triangle/hello_triangle.cpp
+++ src/mains/hello_triangle/hello_triangle.cpp
@@ -166,7 +166,8 @@ int main()
         timeValue = glfwGetTime();
         greenValue = sin(timeValue) / 2.0f + 0.5f;
         vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 1.0f - greenValue, 1.0f);
+        glUniform4f(vertexColorLocation, 0.0f, static_cast<GLfloat>(greenValue),
+            static_cast<GLfloat>(1.0f - greenValue), 1.0f);
 
         // as we only have a single VAO there's no need to bind it every time,
         // but we'll do so to keep things a bit more organized
