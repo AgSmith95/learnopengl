3ebd253ce1c5e7ee0abbc89513c92eaf8d41b8b2
AgSmith95 <ivan.a.kornienko@gmail.com>
2021-02-24 18:25:58 +0200

    Textures
    Added simplest example of using textures in OpenGL.
    Added texture image.
    Had to drop using my Shape wrapper as it would require more effort to modify it in a way to use textures as in the example showed in this tutorial.
    
    https://learnopengl.com/Getting-started/Textures

--- src/mains/textures/textures.cpp
+++ src/mains/textures/textures.cpp
@@ -4,6 +4,7 @@
 #include <glad/glad.h> // WARNING! INCLUDE BEFORE GLFW!!!
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
 #include <helpers.h>
@@ -17,38 +18,76 @@ int main()
     // Init GL by initializing Wrapper singleton
     window(); // not very elegant, yeah
 
-    // SHAPE
-    Shape shape(
-            {
-                    -0.5f, -0.5f, 0.0f,
-                     0.0f,  0.5f, 0.0f,
-                     0.5f, -0.5f, 0.0f
-            },
-            {
-                    0, 1, 2
-            }
-    );
 
     // SHADERS
     Shader ourShader(
-            SHADERS_DIRECTORY "shaders/vertex2.vsh",
-            SHADERS_DIRECTORY "shaders/fragment2.fsh"
+            SHADERS_DIRECTORY "textures/texture_vsh.vsh",
+            SHADERS_DIRECTORY "textures/texture_fsh.fsh"
     );
 
-    // tmp values
-    double timeValue = 0.0f;
-    float  greenValue = 0.0f;
+    // SHAPE
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+            // positions          // colors           // texture coords
+            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+    };
+    unsigned int indices[] = {
+            0, 1, 3, // first triangle
+            1, 2, 3  // second triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // generating texture
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    // bind texture
+    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load texture image
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(RESOURCE_DIRECTORY "textures/container.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load texture\n";
+        return 1;
+    }
+    stbi_image_free(data);
+
     // RENDER LOOP
     while(!glfwWindowShouldClose(window()))
     {
-        // use our shader
-        ourShader.use();
-
-        // update the uniform color
-        timeValue = glfwGetTime();
-        greenValue = static_cast<float>(sin(timeValue)) / 2.0f + 0.5f;
-        ourShader.setFloat4("ourColor", {0.0f, greenValue, 1.0f - greenValue, 1.0f});
-
         // processing input
         processInput(window());
 
@@ -56,13 +95,27 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        shape.Draw();
+        // bind Texture
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        // use our shader
+        ourShader.use();
+
+        // draw shape
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // swap buffers
         glfwSwapBuffers(window());
         // check and call events
         glfwPollEvents();
     }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     return 0;
 }
