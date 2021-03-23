f26f544e6d3ae8908b91179bd493cd44ebee9a61
AgSmith95 <ivan.a.kornienko@gmail.com>
2021-03-24 00:26:10 +0200

    Experiments rcubes
    refactoring?

--- src/mains/experiments/rotating_cubes/rotating_cubes.cpp
+++ src/mains/experiments/rotating_cubes/rotating_cubes.cpp
@@ -144,7 +144,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load(ROTATING_CUBES_DIR "loopheroAA.png", &width, &height, &nrChannels, 0);
+    data = stbi_load(RESOURCE_DIRECTORY "textures/loopheroAA.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         // note that the awesomeface.png has transparency and thus an alpha channel,
--- src/mains/experiments/rotating_cubes/traffic_lights/traffic_lights.cpp
+++ src/mains/experiments/rotating_cubes/traffic_lights/traffic_lights.cpp
@@ -150,7 +150,7 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char *data = stbi_load(TRAFFIC_LIGHTS_DIR "Red_YellowBorder.png", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load(RESOURCE_DIRECTORY "textures/ed_YellowBorder.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -175,7 +175,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load(TRAFFIC_LIGHTS_DIR "Yellow_Green-Border.png", &width, &height, &nrChannels, 0);
+    data = stbi_load(RESOURCE_DIRECTORY "textures/Yellow_Green-Border.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -200,7 +200,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load(TRAFFIC_LIGHTS_DIR "Green_Red-Border.png", &width, &height, &nrChannels, 0);
+    data = stbi_load(RESOURCE_DIRECTORY "textures/Green_Red-Border.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         // note that the awesomeface.png has transparency and thus an alpha channel,
